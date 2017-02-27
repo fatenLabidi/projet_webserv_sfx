251f9b0cbfa68fe586d9d2c2a8a720097f6b8203
Faten Labidi <faten.labidi@heig-vd.ch>
2017-02-27 16:54:40 +0100

    test commit

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../models/user');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  User.find().sort('name').exec(function(err, users) {
+    if (err) {
+      return next(err);
+    }
+    res.send(users);
+  });
+});
+
+/* POST new user */
+router.post('/', function(req, res, next) {
+  // Create a new document from the JSON in the request body
+  const newUser = new User(req.body);
+  // Save that document
+  newUser.save(function(err, savedUser) {
+    if (err) {
+      return next(err);
+    }
+    // Send the saved document in the response
+    res.send(savedUser);
+  });
+});
+
+module.exports = router;
