df23c96f8adbee6a55c9f5bb04dd8db8d85f47ef
Xavier <xavier.aeby@outlook.com>
2017-03-01 11:23:08 +0100

    Creation du Model Issue

--- models/issue.js
+++ models/issue.js
@@ -1,8 +1,64 @@
 const mongoose = require('mongoose');
+const User = require("./user");
+const ObjectId = mongoose.Types.ObjectId;
 const Schema = mongoose.Schema;
 // Define the schema for users
 const issueSchema = new Schema({
-  name: String
+  status: {
+    type: String,
+    required: true,
+    enum :['new', 'inProgress', 'completed', 'canceled']
+  },
+  description:{
+    type: String,
+    maxlength: [ 1000, 'Description is too long' ]
+  },
+  imageUrl : {
+    type: String,
+    maxlength: 50
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  tags: [{ type: String, minlength: 3 }],
+  user: {
+    type: Schema.Types.ObjectId,
+    required:true,
+    ref: 'Person',
+    validate: {
+      // valide que le user existe avec la fonction existingUser
+      validator: existingUser
+    }
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
 });
+
+/**
+ * fonction qui valide si le user existe, via son id
+ */
+function existingUser(value, callback) {
+  User.findOne({ '_id': value }, function (err, user){
+    if (user){
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+
 // Create the model from the schema and export it
 module.exports = mongoose.model('Issue', issueSchema);
